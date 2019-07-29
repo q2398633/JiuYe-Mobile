3d998bead1ced7f664886b557ee532faa25dc9b4
耿源 <2788816975@qq.com>
2019-07-29 19:11:45 +0800

    项目初始化, 加载依赖,清除垃圾代码

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import VeeValidate, { Validator } from 'vee-validate'
+import 'vant/lib/index.css'
+import 'amfe-flexible/index.min.js'
+
+import zh_CN from 'vee-validate/dist/locale/zh_CN'
+
+Vue.use(Vant)
+Vue.use(VeeValidate)
+
+Validator.localize('zh_CN', zh_CN)
+
+// 自定义校验规则
 
 Vue.config.productionTip = false
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        <h1>APP</h1>
+        <van-button type="default">默认按钮</van-button>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
